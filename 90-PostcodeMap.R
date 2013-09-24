7098eb23fbb6cbf2e972175080e02284463ebc1e
mngu2382 <mngu2382@gmail.com>
2013-09-24 19:46:00 +1000

    cleaned up postcode plot code

--- 90-PostcodeMap.R
+++ 90-PostcodeMap.R
@@ -25,11 +25,10 @@ div2 <- div2[match(unique(div2$DIVISION), div2$DIVISION),]
 pltCol <- sapply(div2$DIVISION,
                  function(s) min(subset(pcLoc, Locality==s)$colCode))
 
-png("figures/postcodemap.png", height=1000, width=660, res=180, pointsize=8)
+png("figures/postcodemap.png", height=900, width=630, res=96)
 par(mar=c(0, 0, 3, 0))
 plot(div2, col=pal[pltCol], lwd=0.2)
-title("Suburbs of Northern Canberra", font.main=1, col.main="#dd3322",
-      cex.main=0.9)
+title("Suburbs of Northern Canberra", font.main=1, col.main="#dd3322")
 text(coordinates(div2), labels=div2$DIVISION, cex=0.6)
 legend("bottomright", legend=postcode, fill=pal[1:4],
        bty="n", inset=0.03, title="Postcode", cex=0.8)
